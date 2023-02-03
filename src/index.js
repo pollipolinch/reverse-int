a69e6a67da440396caa9736d50aadb519f0df8d7
Полина <a1@MacBook-Air-Polina.local>
2023-02-04 01:29:47 +0500

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if(n>0){
+        let newN = n.toString().split('')
+        result = ''
+        for(let i = newN.length-1;i>=0;i--){
+           result=result+newN[i]
+        }return Number(result)  
+    }else{
+        n= n*(-1)
+        let newN =n.toString().split('')
+               result = ''
+               for(let i = newN.length-1;i>=0;i--){
+                  result=result+newN[i]
+               }return Number(result)
+    }
+    
 }
